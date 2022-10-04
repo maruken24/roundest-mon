c21c861750c29bf75eb343b0b277c3cb3b3840fd
maruken <maruken@MacBook-Pro-3.local>
2022-10-04 16:33:56 +0900

    Add default page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <p className="text-2xl text-red-500 font-bold">hello world!</p>;
+  return <p className="text-2xl">hello world!</p>;
 };
 
 export default Home;
