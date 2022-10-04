7eb7210d4d376f4c466dfde8c4c67fdc580156b2
maruken <maruken@MacBook-Pro-3.local>
2022-10-04 18:44:03 +0900

    Add random pokemon fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const [first, second] = getOptionsForVote();
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
